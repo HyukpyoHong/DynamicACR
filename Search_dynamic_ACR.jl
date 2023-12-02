6c29dae4147cbfe70a4ceb13f23b7fd11696a404
HyukpyoHong <hphong@kaist.ac.kr>
2023-12-01 22:14:47 -0600

    Modularize the code through a significant revision.

--- Search_dynamic_ACR.jl
+++ Search_dynamic_ACR.jl
@@ -10,7 +10,8 @@ using DataFrames
 using DifferentialEquations
 using Interpolations
 using Plots
-using StatsBase, Statistics
+# using PlotlyJS
+using StatsBase, Statistics, Distributions
 using LinearAlgebra
 
 eps_acr = 0.01 # the threshold value for checking ACR. 
@@ -22,18 +23,16 @@ num_S = 3 # number of species
 max_order = 2 # the maximum order of reaction. e.g, 1: monomolecular, 2:bimolecular, ...
 num_R = 3 # the number of reactions that a random network contains.
 
-num_repeat_net = 100 # the number of random networks that you want to search/check.
-num_repeat_par = 10 # the number of parameter sets that you want to check ACR property.
-num_repeat_init = 100 # the number of initial condition sets for a given fixed parameter set to check ACR property.
+num_repeat_net = 5 # the number of random networks that you want to search/check.
+num_repeat_par = 2 # the number of parameter sets that you want to check ACR property.
+num_repeat_init = 10 # the number of initial condition sets for a given fixed parameter set to check ACR property.
 
 source_mat = nothing # Initialization of a variable that stores source complexes of a network.
 product_mat = nothing # Initialization of a variable that stores product complexes of a network. 
 stoi_mat = nothing  # Initialization of a variable for a stoichiometric matrix. 
-# These initializations is necessary to use it as a global variable outside the for loops.  
+# These initializations are necessary to use them as global variables outside of the for loops.  
 
 # Define functions at the top of the code. 
-# We have to check if this is valid: 
-# e.g., the global variable stoi_mat functions properly in this code flow?
 function MAK(dx, x, kappa, t)
     # f: vector of the rate functions WITHOUT rate constants.
     # kappa: vector of the rate constants
@@ -67,21 +66,8 @@ function MAK_rsc(dx, x, kappa, t)
 end
 
 num_C = binomial(max_order + num_S, num_S)
-
-tmp_cmplx = fill(0, num_S)
-total_complex = fill(0, num_S, num_C)
-
-for i in 1:num_C
-    for j in 1:num_S
-        if sum(tmp_cmplx) > max_order
-            tmp_cmplx[j] = 0
-            tmp_cmplx[j+1] = tmp_cmplx[j+1] + 1
-        end
-    end
-    total_complex[:, i] = tmp_cmplx
-    tmp_cmplx[1] = tmp_cmplx[1] + 1
-end
-# total_complex
+total_complex = make_total_complexes(num_S, max_order)
+# total_complex contains all possible complexes under the max_order (e.g., bimolecular, trimolecular, ...)
 
 ## Generate a (mono or bimolecular) random network with the given numbers of species and reactions (num_S and num_R)
 num_total_R = num_C * (num_C - 1)
@@ -94,40 +80,18 @@ for iter_network in 1:num_repeat_net
     list_source_id = fill(0, num_R)
     list_product_id = fill(0, num_R)
     for i in 1:num_R
-        sr_i = Int(floor((list_R_id[i] - 1) / (num_C - 1))) + 1
-        pd_i = mod((list_R_id[i] - 1), (num_C - 1)) + 1
-        if sr_i <= pd_i
-            pd_i = pd_i + 1
-        end
-        list_source_id[i] = sr_i #Int(sr_i)
-        list_product_id[i] = pd_i
+        s_id, p_id = reaction_to_complex(list_R_id[i], num_C)
+        list_source_id[i] = s_id
+        list_product_id[i] = p_id
     end
-
     source_mat = total_complex[:, list_source_id]
     product_mat = total_complex[:, list_product_id]
-    stoi_mat = product_mat - source_mat
+    stoi_mat = product_mat - source_mat # stoichiometry matrix
 
     # these lines for searching only networks with conservation laws.
-    if rank(stoi_mat) == num_S
-        continue
-    end
-    ## One can manually specify the reaction network using this part. 
-
-    # start comment - examples, use #= (multiline comments) =#
-    # example 1: A+B -> 2B, B -> A
-    #source_mat = [1 1; 0 1]' # source complex matrix
-    #product_mat = [0 2; 1 0]' # 
-    # example 2: 2A+B -> 2B, B -> A
-    #source_mat = [2 1; 0 1]'
-    #product_mat = [0 2; 1 0]'
-    # example 3: A+B -> 2B, B -> A, 3A+B -> 2A+2B, 2A+B -> 3A.
-    #source_mat = [1 1; 0 1; 3 1; 2 1]'
-    #product_mat = [0 2; 1 0; 2 2; 3 0]'
-    # end comment - examples 
-
-    stoi_mat = product_mat - source_mat # stoichiometry matrix
-    # num_S = size(source_mat)[1]
-    # num_R = size(source_mat)[2]
+    # if rank(stoi_mat) == num_S
+    #     continue
+    # end
 
     ## simulation settings: initial conditions, parameters, and the time interval.
     kappa1 = zeros(num_R, 1) # vector of rate constants. It must be a column vector.
@@ -140,7 +104,7 @@ for iter_network in 1:num_repeat_net
     ub_init = 1000 * ones(num_S, 1) # vector of the upper bounds for (randomly generated) initial conditions. 
     lb_init = 10 * ones(num_S, 1) # vector of the lower bounds for (randomly generated) initial conditions. 
 
-    list_acr_id_par = fill(0, num_S, num_repeat_par)
+    list_acr_id_par = fill(0, num_S, num_repeat_par) # (i,j)-entry is 1 if the i-th species shows the dynamic ACR under the j-th parameter set.
     for iter_par in 1:num_repeat_par
         for i in 1:num_R
             kappa1[i] = lb_param[i] + (ub_param[i] - lb_param[i]) * rand()
@@ -156,13 +120,10 @@ for iter_network in 1:num_repeat_net
             prob1 = ODEProblem(MAK_rsc, x_init, tspan1, kappa1) # Use the rescaled mass-action kinetics
             #prob1 = ODEProblem(MAK, x_init, tspan1, kappa1) # Use the original mass-action kinetics
 
-            #add_callback!(prob1, PositiveDomain())
             abstol = [Inf, 1e-1, Inf]
             # reltol = [1e-1, Inf, Inf]
 
             sol1 = solve(prob1, Tsit5(), saveat=1) # save the solution with the interval 'saveat'. saveat would be 1 or 0.1. 
-            # sol1 = solve(prob1);
-            # plot(sol1)
             sol_mat1 = reduce(hcat, sol1.u)'
             length_tspan = length(sol1.t)
             final_val_mat[:, iter_init] = sol_mat1[length_tspan, :]
@@ -171,12 +132,9 @@ for iter_network in 1:num_repeat_net
             end
         end
 
-        #plot(sol1.t, sol_mat1)
-        #plot(sol_mat1[:, 1], sol_mat1[:, 2])
-
         # Find quantiles
-        upp_val = fill(0, num_S)
-        low_val = fill(0, num_S)
+        upp_val = zeros(num_S)
+        low_val = zeros(num_S)
         for i in 1:num_S
             upp_val[i] = quantile(final_val_mat[i, :], 0.6) # 40th out of 100 for the i-th species
             low_val[i] = quantile(final_val_mat[i, :], 0.4) # 60th out of 100 for the i-th species      
@@ -189,15 +147,15 @@ for iter_network in 1:num_repeat_net
         end
     end
     for i in 1:num_S
-    if (sum(list_acr_id_par[i,:]) > 0)
-        list_acr_id[i,iter_network] = list_acr_id[i,iter_network] + 1 
-        # partial ACR (ACR for a subset of parameter sets)
+        if (sum(list_acr_id_par[i, :]) > 0)
+            list_acr_id[i, iter_network] = list_acr_id[i, iter_network] + 1
+            # partial ACR (ACR for a subset of parameter sets)
+        end
+        if (prod(list_acr_id_par[i, :]) > 0)
+            list_acr_id[i, iter_network] = list_acr_id[i, iter_network] + 1
+            # complete ACR (ACR for a subset of parameter sets)
+        end
     end
-    if (prod(list_acr_id_par[i,:]) > 0)
-        list_acr_id[i,iter_network] = list_acr_id[i,iter_network] + 1
-        # complete ACR (ACR for a subset of parameter sets)
-    end
-end
     matrix_R_id[:, iter_network] = list_R_id
 end
 
@@ -210,52 +168,35 @@ CSV.write(path * "matrix_R_id_S" * string(num_S) * "R" * string(num_R) * ".csv",
 # CSV.write(path * "list_acr_id_S" * string(num_S) * "R" * string(num_R) * "_nonfullrank.csv", Tables.table(list_acr_id), writeheader=false)
 # CSV.write(path * "matrix_R_id_S" * string(num_S) * "R" * string(num_R) * "_nonfullrank.csv", Tables.table(matrix_R_id), writeheader=false)
 
-
 # These lines below are for loading saved data
 #xx = CSV.read(path * "list_acr_id_S" * string(num_S) * "R" * string(num_R) * ".csv", DataFrame, header = false)
 #mm = CSV.read(path * "matrix_R_id_S" * string(num_S) * "R" * string(num_R) * ".csv", DataFrame, header = false)
 #list_acr_id = xx[:,1]
 #matrix_R_id = Matrix(mm)
 
-net_list_with_acr = findall(list_acr_id[1,:] .> 0);
+net_list_with_acr = findall(list_acr_id[1, :] .> 0);
 
 for check_net_id in net_list_with_acr ## the code below is now designed for one instance of reaction network not for loop.
-
     #check_net_id = net_list_with_acr[5]
     list_R_id = matrix_R_id[:, check_net_id]
     list_source_id = fill(0, num_R)
     list_product_id = fill(0, num_R)
-
     for i in 1:num_R
-        sr_i = Int(floor((list_R_id[i] - 1) / (num_C - 1))) + 1
-        pd_i = mod((list_R_id[i] - 1), (num_C - 1)) + 1
-        if sr_i <= pd_i
-            pd_i = pd_i + 1
-        end
-        list_source_id[i] = sr_i #Int(sr_i)
-        list_product_id[i] = pd_i
+        s_id, p_id = reaction_to_complex(list_R_id[i], num_C)
+        list_source_id[i] = s_id
+        list_product_id[i] = p_id
     end
     source_mat = total_complex[:, list_source_id]
     product_mat = total_complex[:, list_product_id]
 
-    # The code below is the module for "writing down" a reaction network. 
+    # The function below is for "writing down" a reaction network. 
     network_txt, network_txt_short = acr.crn_writing(source_mat, product_mat)
-
-    # println(network_txt_short)
-    # rank(stoi_mat)
-    # if rank(stoi_mat) < num_S
-    #     println(check_net_id)
-    #     break
-    # end
-
-    # kernel(stoi_mat)
-    # L_ker = nullspace(stoi_mat')
-    # #L_ker = nullspace(s');
-    # if length(L_ker) > 0
-    #     L_ker_norm = L_ker / L_ker[1]
-    #     # Int.(round.(L_ker_norm))
-    # end
 end
+
+
+ss1 = acr.crn_embedding(source_mat, product_mat);
+ss2 = acr.crn_embedding(product_mat, source_mat);
+plot(ss1, ss2, layout = (2,2))
 
 ## Should set a convergence criterion later.
 # Solve the ODE
@@ -265,11 +206,6 @@ plot!(1:num_repeat_init, latter_val_mat[1, :], linewidth=2)
 plot(1:num_repeat_init, final_val_mat[2, :],
     xtickfont=12, ytickfont=12, linewidth=2)
 plot!(1:num_repeat_init, latter_val_mat[2, :], linewidth=2)
-scatter(final_val_mat[1, :], final_val_mat[2, :], alpha=0.3)
-scatter!(final_val_mat[1, :], final_val_mat[2, :], alpha=0.3)
 
-# plot(1:num_repeat_init, final_val_mat[3,:])
-# plot!(1:num_repeat_init, latter_val_mat[3,:])
-a,b = acr.crn_writing(source_mat, source_mat)
-a
-b
+source_mat = zeros(3,0)
+source_mat = hcat(source_mat, [1;2;3])
--- source_ACR.jl
+++ source_ACR.jl
@@ -1,8 +1,9 @@
 module source_ACR
+using Plots: plot, plot!
 
 function crn_writing(source_mat, product_mat)
     num_S, num_R = size(source_mat)
-    
+
     source_txt = fill("", num_R)
     product_txt = fill("", num_R)
     source_txt_short = fill("", num_R)
@@ -21,7 +22,7 @@ function crn_writing(source_mat, product_mat)
                 source_txt[i] = source_txt[i] * "+"
                 product_txt[i] = product_txt[i] * "+"
             end
-    
+
             # create a short version of strings, e.g., 3B+C instead of 0A+3B+1C 
             if source_mat[j, i] > 1
                 source_txt_short[i] = source_txt_short[i] * string(source_mat[j, i]) * string(Char(64 + j))
@@ -31,7 +32,7 @@ function crn_writing(source_mat, product_mat)
                 plus_flag_source = 1
                 # omit "1" for the coefficient, i.e., use "A" instead of "1A"
             end
-    
+
             if product_mat[j, i] > 1
                 product_txt_short[i] = product_txt_short[i] * string(product_mat[j, i]) * string(Char(64 + j))
                 plus_flag_product = 1
@@ -56,12 +57,63 @@ function crn_writing(source_mat, product_mat)
             product_txt_short[i] = "0" # add "0" is the product complex of a reaction is the zero complex.
         end
     end
-    
+
     network_txt = source_txt .* fill(" -> ", num_R) .* product_txt
     network_txt_short = source_txt_short .* fill(" -> ", num_R) .* product_txt_short
-    
+
     return network_txt, network_txt_short
 
+end
+
+function crn_embedding(source_mat, product_mat)
+    num_S, num_R = size(source_mat)
+    if num_S == 2
+        p = plot([0.0 3.0], [0.0 3.0])
+        for i in 1:num_R
+            plot!([source_mat[1, i], product_mat[1, i]],
+                [source_mat[2, i], product_mat[2, i]],
+                arrow=true, color=:black, linewidth=2, label="")
+        end
+    elseif num_S == 3
+        p = plot([0.0 3.0], [0.0 3.0], [0.0 3.0])
+        for i in 1:num_R
+            plot!([source_mat[1, i], product_mat[1, i]],
+                [source_mat[2, i], product_mat[2, i]],
+                [source_mat[3, i], product_mat[3, i]],
+                arrow=true, color=:black, linewidth=2, label="")
+        end
+    else
+        println("Visualization is possible only when the number of species is either 2 or 3")
+        return nothing
+    end
+    return p
+end
+
+function make_total_complexes(num_S, max_order)
+    num_C = binomial(max_order + num_S, num_S)
+    tmp_cmplx = fill(0, num_S)
+    total_complex = fill(0, num_S, num_C)
+
+    for i in 1:num_C
+        for j in 1:num_S
+            if sum(tmp_cmplx) > max_order
+                tmp_cmplx[j] = 0
+                tmp_cmplx[j+1] = tmp_cmplx[j+1] + 1
+            end
+        end
+        total_complex[:, i] = tmp_cmplx
+        tmp_cmplx[1] = tmp_cmplx[1] + 1
+    end
+    return total_complex
+end
+
+function reaction_to_complex(r_id, num_C)
+    source_id = Int(floor((r_id - 1) / (num_C - 1))) + 1
+    product_id = mod((r_id - 1), (num_C - 1)) + 1
+    if source_id <= product_id
+        product_id = product_id + 1
+    end
+    return source_id, product_id
 end
 
 end
