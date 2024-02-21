b5e85dd2d62ce03e029e0fc53cd0fbef1d7b6a0a
Hyukpyo Hong <hhong78@wisc.edu>
2024-02-20 23:55:46 -0600

    Major changes. Add many functions to create the function that finds all networks' IDs which are equivalent to the given network ID. Conversion between ID, complexes, reactions are now available.

--- source_ACR_py.py
+++ source_ACR_py.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import math
 from scipy.special import binom
 from mpl_toolkits.mplot3d import Axes3D
+from sympy.utilities.iterables import multiset_permutations
 
 def MAK(t, x, kappa, source_mat, stoi_mat):
     f = np.zeros(len(kappa))
@@ -37,16 +38,33 @@ def make_total_complexes(num_S, max_order):
         
     return total_complex
 
-def reaction_to_complex(r_id, num_total_C):
-    source_id = int(np.floor((r_id - 1) / (num_total_C - 1))) + 1
-    product_id = int(np.mod((r_id - 1), (num_total_C - 1))) + 1
+def complex_to_complex_id(cmplx, num_S, max_order):
+    num_total_C = int(binom(max_order + num_S, num_S)) # == total_complex.shape[1]
+    total_complex = make_total_complexes(num_S, max_order)
+    
+    # brute force searching
+    for cmplx_id in range(1,num_total_C+1):
+        if np.prod(total_complex[:,cmplx_id-1] == cmplx):
+            return cmplx_id
+
+def reaction_id_to_complex_id(R_id, num_total_C):
+    source_id = int(np.floor((R_id - 1) / (num_total_C - 1))) + 1
+    product_id = int(np.mod((R_id - 1), (num_total_C - 1))) + 1
 
     if source_id <= product_id:
         product_id += 1
 
     return source_id, product_id
 
-def net_id_to_r_id_list(net_id, num_total_R, num_R):
+def complex_id_to_reaction_id(source_id, product_id, num_total_C):
+    reaction_id = (source_id - 1)*(num_total_C-1) + product_id
+    
+    if source_id <= product_id:
+        reaction_id -= 1
+    
+    return reaction_id
+
+def net_id_to_R_id_list(net_id, num_total_R, num_R):
     tmp_id = net_id
     reaction_id_list = np.zeros(num_R, dtype=int) # the list contains the id for reaction r_1, r_2, ..., r_{num_R}.
     # Those id's have to satisfy 1 <= r_1 < r_2 < ... < r_{num_R} <= num_total_R.
@@ -74,6 +92,29 @@ def net_id_to_r_id_list(net_id, num_total_R, num_R):
                 break
 
     return reaction_id_list
+
+def R_id_list_to_net_id(R_id_list_in, num_total_R):
+    R_id_list = np.sort(R_id_list_in)
+    num_R = len(R_id_list)
+    num_total_net = int(binom(num_total_R, num_R))
+    single_id = np.sum(R_id_list * (((num_total_R+1) * np.ones(num_R)) ** np.flip(range(num_R))))
+    # this single_id is a unique identifier for R_id_list
+
+    search_lb = 1 # lower bound of the searching interval
+    search_ub = num_total_net # upper bound of the searching interval
+    
+    # perform binary search to find the correct net_id
+    while True:
+        search_net_id = int(np.floor((search_lb + search_ub)/2))
+        tmp_R_id_list = net_id_to_R_id_list(search_net_id, num_total_R, num_R)
+        tmp_single_id = np.sum(tmp_R_id_list * (((num_total_R+1) * np.ones(num_R)) ** np.flip(range(num_R))))
+        #print(search_net_id)
+        if tmp_single_id == single_id:
+            return search_net_id
+        elif tmp_single_id > single_id:
+            search_ub = search_net_id - 1
+        else:
+            search_lb = search_net_id + 1
 
 def crn_writing(source_mat, product_mat):
     num_S, num_R = source_mat.shape
@@ -257,7 +298,7 @@ def crn_embedding_info(source_mat, product_mat, acr_id, unbnd_id, shake=True):
     # plt.show()
     return fig, ax
 
-def add_CRN_to_Axes(axes, row, col, source_mat, product_mat, acr_id, unbnd_id):
+def add_CRN_to_Axes(axes, row, col, source_mat, product_mat, acr_id, unbnd_id, net_id):
     ax = axes[row,col]
     num_S, num_R = source_mat.shape
     axes_lim = [-0.1, 2.1]
@@ -289,7 +330,7 @@ def add_CRN_to_Axes(axes, row, col, source_mat, product_mat, acr_id, unbnd_id):
             ax.arrow(source_mat[0, i] + move[0], source_mat[1, i] + move[1],
                      product_mat[0, i] - source_mat[0, i], product_mat[1, i] - source_mat[1, i],
                      head_width=0.15, head_length=0.15, length_includes_head = True, fc='black', ec='black')
-        ax.text(1.2, 1.4, f"ACR: {acr_sp}\nUnb: {unbnd_sp}\ndim(S): {r}", fontsize=9)
+        ax.text(1.2, 1.4, f"ACR: {acr_sp}\nUnb: {unbnd_sp}\ndim(S): {r}\nID: {net_id}", fontsize=9)
     elif num_S == 3:
         text_id_acr = acr_id_tf[0] * 1 + acr_id_tf[1] * 2 + acr_id_tf[2] * 4
         acr_sp = ["∅", "A", "B", "A,B", "C", "A,C", "B,C", "A,B,C"][text_id_acr]
@@ -315,13 +356,11 @@ def add_CRN_to_Axes(axes, row, col, source_mat, product_mat, acr_id, unbnd_id):
             ax.quiver(source_mat[0, i] + move[0], source_mat[1, i] + move[1], source_mat[2, i] + move[2],
                       product_mat[0, i] - source_mat[0, i], product_mat[1, i] - source_mat[1, i],
                       product_mat[2, i] - source_mat[2, i], color='black', linewidth=1.5)
-        ax.text(1.2, 1.4, 1.4, f"ACR: {acr_sp}\nUnb: {unbnd_sp}\ndim(S): {r}", fontsize=9)
+        ax.text(1.2, 1.4, 1.4, f"ACR: {acr_sp}\nUnb: {unbnd_sp}\ndim(S): {r}\nID: {net_id}", fontsize=9)
     else:
         print("Visualization is possible only when the number of species is either 2 or 3. 'None' is returned")
         return None
     return None
-
-
 
 def grouping_network(source_mat, product_mat, acr_id, unbnd_id):
     num_S = source_mat.shape[0]
@@ -347,3 +386,32 @@ def grouping_network(source_mat, product_mat, acr_id, unbnd_id):
         return None
 
     return gp_str
+
+def find_equiv_network_ids(net_id, num_S, max_order, num_R):
+
+    num_total_C = int(binom(max_order + num_S, num_S))
+    total_complex = make_total_complexes(num_S, max_order)
+
+    num_total_R = num_total_C * (num_total_C - 1)
+    num_total_net = int(binom(num_total_R, num_R))
+    list_R_id = net_id_to_R_id_list(net_id, num_total_R, num_R)
+    list_source_id, list_product_id = zip(*[reaction_id_to_complex_id(R_id, num_total_C) for R_id in list_R_id])
+    source_mat = total_complex[:, np.array(list_source_id)-1]
+    product_mat = total_complex[:, np.array(list_product_id)-1]
+
+    S_sorted = np.array(range(num_S))
+    equiv_net_ids = np.array([], dtype=int)
+    for S_permuted in multiset_permutations(S_sorted):
+        new_source_mat = source_mat[S_permuted,:]
+        new_product_mat = product_mat[S_permuted,:]
+        new_list_source_id = [complex_to_complex_id(new_source_mat[:,i], num_S, max_order) for i in range(num_R)]
+        new_list_product_id = [complex_to_complex_id(new_product_mat[:,i], num_S, max_order) for i in range(num_R)]
+        new_list_R_id = [complex_id_to_reaction_id(new_list_source_id[i], new_list_product_id[i], num_total_C) 
+                         for i in range(num_R)]
+        new_net_id = R_id_list_to_net_id(new_list_R_id, num_total_R)
+        equiv_net_ids = np.append(equiv_net_ids, new_net_id)
+
+    return equiv_net_ids
+    
+    
+    
